91e16b0fb65891126e38365d68a651f2c745521d
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-04-21 13:15:16 +0100

    testing quality states

--- ruby/lib/regular_item.rb
+++ ruby/lib/regular_item.rb
@@ -1,6 +1,14 @@
 require_relative "item"
 
+#using abstraction and class inheritence to make code easeir to read
 class RegularItem < Item
 
+  def alter_quality
+    if @quality > 0
+      @sell_in -=1
+      @quality -=1
+    end
+    @quality
+  end
 
 end
--- ruby/spec/regular_items_spec.rb
+++ ruby/spec/regular_items_spec.rb
@@ -9,4 +9,21 @@ describe RegularItem do
     expect(item.name).to eq("Test Item")
   end
 
+  it "after one day, the SellIn value gets lowered" do
+    item = RegularItem.new("Test Item", 1, 1)
+    GildedRose.new(item).update_quality
+    expect(item.sell_in).to eq(0)
+  end
+
+  it "will not decrease the quality of an item if it is already 0" do
+    item = RegularItem.new("Test Item", 1, 0)
+    GildedRose.new(item).update_quality
+    expect(item.quality).to eq(0)
+  end
+
+  it "will decrease the quality of an item if its quality is greater than 0" do
+    item = RegularItem.new("Test Item", 1, 5)
+    GildedRose.new(item).update_quality
+    expect(item.quality).to eq(4)
+  end
 end
