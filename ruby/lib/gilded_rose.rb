d4aa3405a89e63c849016df66f58d4d0fc6f890b
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-04-21 12:54:28 +0100

    abstracted the item class

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -1,82 +1,57 @@
+require_relative 'item'
 class GildedRose
-attr_reader :items, :name, :quality
+# attr_reader :items
 
   def initialize(items)
-    @items = items
-    @quality = @items.quality.to_i
-    @name = @items.name.to_s
+    @items = []
+
   end
 
   def update_quality()
-    p @items
-
-    # @items.each do |item|
-      if @name == "Aged Brie" && @quality < 50
-         @quality += 1
-       elsif @quality == 0 ||@quality == 50 || @name == "Sulfuras"
-         @quality
-       else
-         @quality -= 1
+    @items.each do |item|
+      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.quality > 0
+          if item.name != "Sulfuras, Hand of Ragnaros"
+            item.quality = item.quality - 1
+          end
+        end
+      else
+        if item.quality < 50
+          item.quality = item.quality + 1
+          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+            if item.sell_in < 11
+              if item.quality < 50
+                item.quality = item.quality + 1
+              end
+            end
+            if item.sell_in < 6
+              if item.quality < 50
+                item.quality = item.quality + 1
+              end
+            end
+          end
+        end
       end
-      @quality
-      @name
-  #     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-  #       if item.quality > 0
-  #         if item.name != "Sulfuras, Hand of Ragnaros"
-  #           item.quality = item.quality - 1
-  #         end
-  #       end
-  #     else
-  #       if item.quality < 50
-  #         item.quality = item.quality + 1
-  #         if item.name == "Backstage passes to a TAFKAL80ETC concert"
-  #           if item.sell_in < 11
-  #             if item.quality < 50
-  #               item.quality = item.quality + 1
-  #             end
-  #           end
-  #           if item.sell_in < 6
-  #             if item.quality < 50
-  #               item.quality = item.quality + 1
-  #             end
-  #           end
-  #         end
-  #       end
-  #     end
-  #     if item.name != "Sulfuras, Hand of Ragnaros"
-  #       item.sell_in = item.sell_in - 1
-  #     end
-  #     if item.sell_in < 0
-  #       if item.name != "Aged Brie"
-  #         if item.name != "Backstage passes to a TAFKAL80ETC concert"
-  #           if item.quality > 0
-  #             if item.name != "Sulfuras, Hand of Ragnaros"
-  #               item.quality = item.quality - 1
-  #             end
-  #           end
-  #         else
-  #           item.quality = item.quality - item.quality
-  #         end
-  #       else
-  #         if item.quality < 50
-  #           item.quality = item.quality + 1
-  #         end
-  #       end
-  #     end
-     end
-   end
-# end
-
-class Item
-  attr_reader :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
+      if item.name != "Sulfuras, Hand of Ragnaros"
+        item.sell_in = item.sell_in - 1
+      end
+      if item.sell_in < 0
+        if item.name != "Aged Brie"
+          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+            if item.quality > 0
+              if item.name != "Sulfuras, Hand of Ragnaros"
+                item.quality = item.quality - 1
+              end
+            end
+          else
+            item.quality = item.quality - item.quality
+          end
+        else
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+      end
+    end
   end
 end
--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,49 +1,54 @@
-# require File.join(File.dirname(__FILE__), 'gilded_rose')
-require "./lib/gilded_rose.rb"
-
-describe GildedRose do
-
-  describe "#update_quality" do
-
-    it "does not change the name" do
-      gilded_rose = GildedRose.new(Item.new("foo", 0, 0))
-      gilded_rose.update_quality()
-      expect(gilded_rose.name).to eq "foo"
-    end
-
-    it "does not decrease the quality of an item if it already at 0" do
-      gilded_rose = GildedRose.new(Item.new("foo", 0, 0))
-      gilded_rose.update_quality()
-      expect(gilded_rose.name).to eq "foo"
-      expect(gilded_rose.quality).to eq(0)
-    end
-
-    it "will decrease the quality of an item that is greater than 0 and not Aged Brie" do
-      gilded_rose = GildedRose.new(Item.new("foo", 1, 2))
-      gilded_rose.update_quality()
-      expect(gilded_rose.name).to eq "foo"
-      expect(gilded_rose.quality).to eq(1)
-    end
-
-    it "the aged brie increases in quality as it gets older" do
-      gilded_rose = GildedRose.new(Item.new("Aged Brie", 0, 0))
-      gilded_rose.update_quality()
-      expect(gilded_rose.name).to eq "Aged Brie"
-      expect(gilded_rose.quality).to eq(1)
-    end
-
-    it "will not increase the quality of Aged Brie if the quality is already 50" do
-      gilded_rose = GildedRose.new(Item.new("Aged Brie", 1, 50))
-      gilded_rose.update_quality()
-      expect(gilded_rose.name).to eq "Aged Brie"
-      expect(gilded_rose.quality).to eq(50)
-    end
-
-    it "the Sulfuras item never has to be sold and never decreases in quality" do
-      gilded_rose = GildedRose.new(Item.new("Sulfuras", 1000, 100))
-      gilded_rose.update_quality()
-      expect(gilded_rose.quality).to eq(100)
-    end
-
-  end
-end
+# # require File.join(File.dirname(__FILE__), 'gilded_rose')
+# require "./lib/gilded_rose.rb"
+#
+# describe GildedRose do
+#
+#   describe "#update_quality" do
+#
+#     it "does not change the name" do
+#       items = Item.new("foo", 0, 0)
+#       GildedRose.new(items)
+#       GildedRose.new(items).update_quality()
+#       expect(items[0].name).to eq "foo"
+#     end
+#
+#     it "does not decrease the quality of an item if it already at 0" do
+#       items = Item.new("foo", 0, 0)
+#       GildedRose.new(items)
+#       GildedRose.new(items).update_quality()
+#       expect(items[0].name).to eq "foo"
+#       expect(items[0].quality).to eq(0)
+#     end
+#
+#     it "will decrease the quality of an item that is greater than 0 and not Aged Brie" do
+#       items = Item.new("foo", 1, 2)
+#       GildedRose.new(items)
+#       GildedRose.new(items).update_quality()
+#       expect(items[0].name).to eq "foo"
+#       expect(items[0].quality).to eq(1)
+#     end
+#
+#     it "the aged brie increases in quality as it gets older" do
+#       items = Item.new("Aged Brie", 0, 0)
+#       GildedRose.new(items)
+#       GildedRose.new(items).update_quality()
+#       expect(items[0].name).to eq "Aged Brie"
+#       expect(items[0].quality).to eq(1)
+#     end
+#
+#     it "will not increase the quality of Aged Brie if the quality is already 50" do
+#       items = Item.new("Aged Brie", 1, 50)
+#       GildedRose.new(items)
+#       GildedRose.new(items).update_quality()
+#       expect(items[0].name).to eq "Aged Brie"
+#       expect(items[0].quality).to eq(50)
+#     end
+#
+#     it "the Sulfuras item never has to be sold and never decreases in quality" do
+#       gilded_rose = GildedRose.new(Item.new("Sulfuras", 1000, 100))
+#       GildedRose.new(items).update_quality()
+#       expect(items[0].quality).to eq(100)
+#     end
+#
+#   end
+# end
