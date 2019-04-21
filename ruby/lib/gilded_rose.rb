bc826c9e54af450f26cfd54cd2d48554a5fbb1e5
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-04-21 12:04:54 +0100

    second passing feature test

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -1,60 +1,74 @@
 class GildedRose
+attr_reader :items, :name, :quality
 
   def initialize(items)
     @items = items
+    @quality = @items.quality.to_i
+    @name = @items.name.to_s
   end
 
   def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+    p @items
+
+    # @items.each do |item|
+      if @name == "Aged Brie"
+         @quality += 1
+       elsif @quality == 0
+         @quality
+       else
+         @quality -= 1
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
-  end
-end
+      @quality
+      @name
+  #     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+  #       if item.quality > 0
+  #         if item.name != "Sulfuras, Hand of Ragnaros"
+  #           item.quality = item.quality - 1
+  #         end
+  #       end
+  #     else
+  #       if item.quality < 50
+  #         item.quality = item.quality + 1
+  #         if item.name == "Backstage passes to a TAFKAL80ETC concert"
+  #           if item.sell_in < 11
+  #             if item.quality < 50
+  #               item.quality = item.quality + 1
+  #             end
+  #           end
+  #           if item.sell_in < 6
+  #             if item.quality < 50
+  #               item.quality = item.quality + 1
+  #             end
+  #           end
+  #         end
+  #       end
+  #     end
+  #     if item.name != "Sulfuras, Hand of Ragnaros"
+  #       item.sell_in = item.sell_in - 1
+  #     end
+  #     if item.sell_in < 0
+  #       if item.name != "Aged Brie"
+  #         if item.name != "Backstage passes to a TAFKAL80ETC concert"
+  #           if item.quality > 0
+  #             if item.name != "Sulfuras, Hand of Ragnaros"
+  #               item.quality = item.quality - 1
+  #             end
+  #           end
+  #         else
+  #           item.quality = item.quality - item.quality
+  #         end
+  #       else
+  #         if item.quality < 50
+  #           item.quality = item.quality + 1
+  #         end
+  #       end
+  #     end
+     end
+   end
+# end
 
 class Item
-  attr_accessor :name, :sell_in, :quality
+  attr_reader :name, :sell_in, :quality
 
   def initialize(name, sell_in, quality)
     @name = name
--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -4,10 +4,20 @@ require "./lib/gilded_rose.rb"
 describe GildedRose do
 
   describe "#update_quality" do
+
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+      gilded_rose = GildedRose.new(Item.new("foo", 0, 0))
+      gilded_rose.update_quality()
+      expect(gilded_rose.name).to eq "foo"
+      expect(gilded_rose.quality).to eq(0)
     end
+
+    it "the aged brie increases in quality as it gets older" do
+      gilded_rose = GildedRose.new(Item.new("Aged Brie", 0, 0))
+      gilded_rose.update_quality()
+      expect(gilded_rose.name).to eq "Aged Brie"
+      expect(gilded_rose.quality).to eq(1)
+    end
+
   end
 end
